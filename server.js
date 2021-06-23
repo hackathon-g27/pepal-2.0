933ef93daf0e3f52f46c8e0d4b12cb43d1681df4
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-06-24 01:56:10 +0200

    fix: corrige le root-dir dans server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./pepal-front/dist/pepal-front"));
+const rootDir = "./pepal-front/dist/pepal-front"
+
+app.use(express.static(rootDir));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/project-name" });
+  res.sendFile("index.html", { root: rootDir });
 });
 
 app.listen(process.env.PORT || 8080);
